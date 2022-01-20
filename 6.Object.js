08f2bd561df6c5b3bba79f219639d39b3fe95185
mailsondias7 <mailsondias.dev@gmail.com>
2022-01-20 17:51:59 -0300

    Object no JS

---
+++ 6.Object.js
@@ -0,0 +1,17 @@
+//Object
+
+/*
+ * Objeto
+ * Propriedades / Atributos
+ * Funcionalidades / Métodos
+  
+  { propriedades: "valor" }
+*/
+
+console.log({
+  name: "Mailson",
+  idade: 32,
+  andar: function(){
+    console.log('andar')
+  }
+})
