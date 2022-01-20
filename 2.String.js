8b8cb1c645b826eb5fa5852e73b7d20fdb725758
mailsondias7 <mailsondias.dev@gmail.com>
2022-01-20 16:11:58 -0300

    String no JS

---
+++ 2.String.js
@@ -0,0 +1,12 @@
+//String
+
+/* 
+ *Cadeia de caracteres
+
+ "" - aspas duplas
+ '' - aspas simples
+ `` - template literal ou template strings
+*/
+
+console.log('Javascript')
+console.log(`Soma = ${1 + 2}`) // Soma = 3
