daa233d3704c01b3ba74cfd71993ba247d8a5bb2
mailsondias7 <mailsondias.dev@gmail.com>
2022-01-26 18:38:53 -0300

    declaration Assignment

---
+++ 7.declarationAssignment.js
@@ -0,0 +1,10 @@
+//Declaration an Types of data
+
+//Declaration - declarar variavel
+var myName
+
+//Assignment - atribuir valores
+var myName = "Mailson"
+
+//Type of data - saber o tipo de dado da variavel
+console.log(typeof myName) //string
