ee8a167158e1162ccafa6862991bf8251aca6ddf
mailsondias7 <mailsondias.dev@gmail.com>
2022-01-27 17:30:53 -0300

    Object no js e renomeando arquivos

---
+++ 8. Object.js
@@ -0,0 +1,12 @@
+//Object
+
+const person = {
+  name: 'Mike',
+  age: 32,
+  weigth: 81.3,
+  isAdmin: true
+}
+
+console.log(person.name) //Mike
+
+console.log(`${person.name} tem ${person.age} anos.`)
