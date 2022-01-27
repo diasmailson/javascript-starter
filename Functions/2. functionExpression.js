d67d4cdcc9c9ceae6b61bfe522438d09c16d3d40
mailsondias7 <mailsondias.dev@gmail.com>
2022-01-27 19:28:38 -0300

    refatorando a funcao

--- Functions/2. functionExpression.js
+++ Functions/2. functionExpression.js
@@ -1,9 +1,10 @@
 // function expression
-//function anonymous
+// function anonymous
 
 //parâmetros (parameters)
 const sum = function(number1, number2) {
-  console.log(number1 + number2)
+  let result = number1 + number2
+  return result
 }
 
 //arguments - argumentos
