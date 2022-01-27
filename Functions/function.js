12e59524563a124bec23bb9986760498f48758f5
mailsondias7 <mailsondias.dev@gmail.com>
2022-01-27 18:46:57 -0300

    function

---
+++ Functions/function.js
@@ -0,0 +1,13 @@
+//Function
+
+//declaration - declaração da função
+//function statement
+
+function creatPhrases() {
+  console.log('Tenha foco!')
+  console.log('Confie que você pode.')
+  console.log('Estude e aplique.')
+}
+
+//Executando a função
+creatPhrases()
